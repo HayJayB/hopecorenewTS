9ca4d85a8ba7a7e39f8acf289341f933959428f8
HayJayB <123222877+HayJayB@users.noreply.github.com>
2025-07-02 23:39:10 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -311,8 +311,6 @@ async function main() {
   await saveListToFile(recentKeywords, RECENT_KEYWORDS_FILE);
 
   console.info(`Posted: ${entry.title}`);
-  console.log("Logged in as DID:", client.session?.did);
-  console.log("Post response:", response);
 }
 
 main().catch(console.error);
