2013f29648ce5afa97f70af0b627f2bb1945bf07
HayJayB <123222877+HayJayB@users.noreply.github.com>
2025-07-02 22:41:42 -0400

    Update feedparser.d.ts

--- feedparser.d.ts
+++ feedparser.d.ts
@@ -1,29 +1,11 @@
 declare module "feedparser" {
   import { EventEmitter } from "events";
 
-  interface Enclosure {
-    url?: string;
-    type?: string;
-  }
-
-  interface Item {
-    title?: string;
-    description?: string;
-    summary?: string;
-    link?: string;
-    pubDate?: Date;
-    date?: Date;
-    image?: { url?: string };
-    enclosures?: Enclosure[];
-  }
-
   class FeedParser extends EventEmitter {
     constructor(options?: any);
-    // Note: the stream pipeline will .pipe() into this
     end(): void;
     write(chunk: any): void;
-    read(): Item | null;
   }
 
-  export = FeedParser;
+  export default FeedParser;
 }
