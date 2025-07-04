ce6307afcd72613ce43fb20802768b7e466693d9
HayJayB <123222877+HayJayB@users.noreply.github.com>
2025-07-04 18:44:03 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,42 @@
 export const MAX_POSTED_LINKS = 50;
-export const MAX_DAYS_OLD = 7;
+export const MAX_DAYS_OLD = 14;
 export const POSITIVE_THRESHOLD = 0.1;
 
 export const RSS_FEEDS = [
-  "https://example.com/feed1.rss",
-  "https://example.com/feed2.rss",
-  // add your progressive RSS feed URLs here
+  "https://jacobin.com/feed",
+  "https://www.dsausa.org/feed/",
+  "https://www.thenation.com/feed/?post_type=article",
+  "https://inthesetimes.com/rss/articles",
+  "https://www.commondreams.org/rss-feed",
+  "https://truthout.org/feed/",
+  "https://progressive.org/feed/",
+  "https://theintercept.com/feed/",
+  "https://commonwealthclub.org/feed/podcast",
+  "https://www.theguardian.com/us-news/us-politics/rss",
+  "https://www.truthdig.com/feed/",
+  "https://www.counterpunch.org/feed/",
+  "https://www.democracynow.org/democracynow.rss",
+  "https://therealnews.com/feed/",
+  "https://labornotes.org/rss.xml",
+  "https://shadowproof.com/feed/",
+  "https://popularresistance.org/feed/",
+  "https://wagingnonviolence.org/feed/",
+  "https://www.leftvoice.org/feed/",
 ];
 
 export const POSITIVE_KEYWORDS = [
-  "progressive",
-  "community",
-  "justice",
-  "green",
-  // etc.
+  "win", "victory", "gains", "success", "growth", "solidarity", "organize", "strike",
+  "socialist", "left wing", "union", "responsibility", "mobilize", "charity",
+  "outreach", "resistance", "community", "truth", "celebrate", "local", "save",
+  "future", "knock", "knocks", "healing", "hope", "love", "progressive", "champion",
+  "leader", "ceasefire",
 ];
 
 export const NEGATIVE_KEYWORDS = [
-  "war",
-  "violence",
-  "hate",
-  // etc.
+  "death", "deadly", "killed", "kill", "killing", "violence", "attack", "crisis",
+  "disaster", "scandal", "accident", "injured", "tragedy", "fraud", "collapse",
+  "bomb", "shooting", "war", "loser", "awful", "horrible", "terrible", "tragic",
+  "destroy", "raiding", "raid", "gut", "fear", "broken", "destruction",
 ];
 
 export const POSTED_LINKS_FILE = "data/posted_links.txt";
